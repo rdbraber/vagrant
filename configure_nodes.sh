0523fa6b7ba8eb60d8f1a233c3039753e1a5477e
Rob den Braber <rdbraber@grid-it.nl>
2015-03-04 08:06:31 +0100

    Changed the way git is installed

--- configure_nodes.sh
+++ configure_nodes.sh
@@ -4,14 +4,6 @@
 
 
 # Install GIT
-#yum -y install zlib-devel perl-CPAN gettext openssl openssl-devel curl-devel gcc
-#mkdir /tmp/git && cd /tmp/git
-#curl https://www.kernel.org/pub/software/scm/git/git-2.0.4.tar.gz | tar xz
-#cd git-2.0.4/
-#./configure
-#make
-#make prefix=/usr install
-#git --version 
 yum -y install git
 
 #Install Puppet
